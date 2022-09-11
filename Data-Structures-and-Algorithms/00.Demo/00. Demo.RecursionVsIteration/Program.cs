30a13c4ac7e931cd26f5bdd4bf53f003807cf2a0
DCay <atriumandvintrigol@gmail.com>
2022-09-11 13:57:10 +0300

    Added combinatorics permutations

---
+++ Data-Structures-and-Algorithms/00.Demo/00. Demo.RecursionVsIteration/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+class Program
+{
+    public static BigInteger RecursiveFactorial(BigInteger n)
+    {
+        if(n == 1)
+        {
+            return 1;
+        }
+
+        return n * RecursiveFactorial(n - 1);
+    }
+
+    public static BigInteger IterativeFactorial(BigInteger n)
+    {
+        BigInteger result = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            result = result * i;
+        }
+
+        return result;
+    }
+
+    public static BigInteger RecursiveFibunacci(BigInteger n)
+    {
+        if(n == 0)
+        {
+            return 0;
+        }
+        else if(n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return RecursiveFibunacci(n - 1) + RecursiveFibunacci(n - 2);
+        }
+    }
+
+    public static BigInteger IterativeFibunacci(BigInteger n)
+    {
+        BigInteger previousPreviousNumber = 1;
+        BigInteger previousNumber = 1;
+        BigInteger currentNumber = 2;
+
+        for (int i = 3; i < n; i++)
+        {
+            previousPreviousNumber = previousNumber;
+            previousNumber = currentNumber;
+            currentNumber = previousPreviousNumber + previousNumber;
+        }
+
+        return currentNumber;
+    }
+
+    public static void Main(string[] args)
+    {
+        BigInteger n = BigInteger.Parse(Console.ReadLine());
+
+        Stopwatch stopwatch = new Stopwatch();
+
+        stopwatch.Start();
+
+        Console.WriteLine(RecursiveFibunacci(n));
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Elapsed: {stopwatch.ElapsedMilliseconds}ms");
+
+        stopwatch.Reset();
+        stopwatch.Start();
+
+        Console.WriteLine(IterativeFibunacci(n));
+
+        stopwatch.Stop();
+
+        Console.WriteLine($"Elapsed: {stopwatch.ElapsedMilliseconds}ms");
+    }
+}
---
+++ Data-Structures-and-Algorithms/00.Demo/00.Demo.Combinatorics/Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    static int steps = 0;
+
+    public static void SwapPermute(
+        int index,
+        string[] set)
+    {
+        if (index >= set.Length)
+        {
+            Console.WriteLine(string.Join(" ", set));
+        }
+        else
+        {
+            SwapPermute(index + 1, set);
+
+            for (int i = index + 1; i < set.Length; i++)
+            {
+                Swap(set, index, i);
+                SwapPermute(index + 1, set);
+                Swap(set, index, i);
+            }
+        }
+    }
+
+    public static void Permute(
+        int index,
+        string[] set,
+        bool[] used,
+        string[] currentPermutation)
+    {
+        if (index >= currentPermutation.Length)
+        {
+            Console.WriteLine(string.Join(" ", currentPermutation));
+        }
+        else
+        {
+            for (int i = 0; i < set.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    currentPermutation[index] = set[i];
+                    Permute(index + 1, set, used, currentPermutation);
+                    used[i] = false;
+                }
+            }
+        }
+    }
+
+    private static void Swap(string[] set, int first, int second)
+    {
+        string temp = set[first];
+        set[first] = set[second];
+        set[second] = temp;
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] set = { "A", "B", "B" };
+
+        Permute(0, set, new bool[set.Length], new string[set.Length]);
+
+        Console.WriteLine($"Steps: {steps}");
+    }
+}
