ee82a6b10a25c27d432d80e0c490567862421c39
DCay <atriumandvintrigol@gmail.com>
2022-09-18 11:38:09 +0300

    Adding DP solutions for Fibunacci and LIS

---
+++ Data-Structures-and-Algorithms/00.Demo/00.Demo.DynamicProgramming/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Numerics;
+using System.Runtime.Intrinsics.Arm;
+
+class Program
+{
+    public static BigInteger RecursiveFibunacci(BigInteger n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            return RecursiveFibunacci(n - 1) + RecursiveFibunacci(n - 2);
+        }
+    }
+
+    public static BigInteger OptimizedFibunacci(int n, BigInteger[] dpArray)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            if (dpArray[n - 1] == 0)
+            {
+                dpArray[n - 1] = OptimizedFibunacci(n - 1, dpArray);
+            }
+
+            if (dpArray[n - 2] == 0)
+            {
+                dpArray[n - 2] = OptimizedFibunacci(n - 2, dpArray);
+            }
+
+
+            return dpArray[n - 1] + dpArray[n - 2];
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        BigInteger[] dpArray = new BigInteger[1000000];
+
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(OptimizedFibunacci(n, dpArray));
+    }
+}
---
+++ Data-Structures-and-Algorithms/00.Demo/00.Demo.LongestIncreasingSubsequence/Program.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq;
+
+class Program
+{
+    public static void RecursiveTraverse(
+        int currentIndex, 
+        int[] sequence, 
+        List<int> currentSubsequence, 
+        List<List<int>> allSubsequences)
+    {
+        if(currentIndex >= sequence.Length)
+        {
+            return;
+        } 
+        else
+        {
+            for (int i = currentIndex; i < sequence.Length; i++)
+            {
+                if (sequence[i] > currentSubsequence[currentSubsequence.Count - 1])
+                {
+                    currentSubsequence.Add(sequence[i]);
+                    RecursiveTraverse(i + 1, sequence, currentSubsequence, allSubsequences);
+                    currentSubsequence.RemoveAt(currentSubsequence.Count - 1);
+                }
+            }
+        } 
+
+        allSubsequences.Add(new List<int>(currentSubsequence));
+    }
+
+    public static void RecursiveSolution(int[] sequence)
+    {
+        List<List<int>> allSubsequences = new List<List<int>>();
+
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            List<int> currentSubsequence = new List<int>();
+            currentSubsequence.Add(sequence[i]);
+            RecursiveTraverse(i + 1, sequence, currentSubsequence, allSubsequences);
+        }
+
+        Console.WriteLine("Longest Increasing Subsequence: ");
+
+        var subsequence = allSubsequences
+            .OrderByDescending(s => s.Count)
+            .FirstOrDefault();
+
+        Console.WriteLine(string.Join(" -> ", subsequence));
+    }
+
+    public static void DPSolution(int[] sequence)
+    {
+        int[] lengths = new int[sequence.Length];
+        int[] previous = new int[sequence.Length];
+
+        int count = 0;
+
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            lengths[i] = 1;
+            previous[i] = -1;
+
+            for (int j = 0; j < i; j++)
+            {
+                count++; 
+                if (sequence[i] > sequence[j])
+                {
+                    previous[i] = j;
+                    lengths[i] = lengths[j] + 1;
+                }
+            }
+        }
+
+        Console.WriteLine("Iterations: " + count);
+
+        int maxLengthIndex = 0;
+        int currentMaxLength = 0;
+
+        for (int i = 0; i < lengths.Length; i++)
+        {
+            if (lengths[i] >= currentMaxLength)
+            {
+                maxLengthIndex = i;
+                currentMaxLength = lengths[i];
+            }
+        }
+
+        List<int> longestIncreasingSubsequence = new List<int>();
+
+        while (maxLengthIndex >= 0)
+        {
+            longestIncreasingSubsequence.Add(sequence[maxLengthIndex]);
+            maxLengthIndex = previous[maxLengthIndex];
+        }
+
+        longestIncreasingSubsequence.Reverse();
+        Console.WriteLine(string.Join(" -> ", longestIncreasingSubsequence));
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        DPSolution(sequence);
+    }
+}
