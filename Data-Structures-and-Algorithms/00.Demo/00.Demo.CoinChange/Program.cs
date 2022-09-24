93e71abdebae459dc725883e1b360fff347f1b1b
DCay <atriumandvintrigol@gmail.com>
2022-09-24 12:03:47 +0300

    Added Rod Cutting and Coin Change Solutions

---
+++ Data-Structures-and-Algorithms/00.Demo/00.Demo.CoinChange/Program.cs
@@ -0,0 +1,62 @@
+﻿class Program
+{
+    public static long Recursive(long currentSum, int index, List<long> coins, long[,] counts)
+    {
+        if(index < 0)
+        {
+            return 0;
+        }
+
+        if(currentSum == 0)
+        {
+            return 1;
+        }
+
+        if (currentSum < 0)
+        {
+            return 0;
+        }
+
+        if (counts[currentSum, index] >= 0)
+        {
+            return counts[currentSum, index];
+        }
+
+        long coin = coins[index];
+
+        long countOfRemaining = Recursive(currentSum - coin, index, coins, counts);
+        long countOfOther = Recursive(currentSum, index - 1, coins, counts);
+
+        counts[currentSum, index] = countOfRemaining + countOfOther;
+
+        return counts[currentSum, index];
+    }
+
+    public static long GetWays(int n, List<long> coins)
+    {
+        coins.Sort(); // 4, 1,2,3
+
+        long[,] counts = new long[n + 1, coins.Count];
+
+        for (int i = 0; i <= n; i++)
+        {
+            for (int j = 0; j < coins.Count; j++)
+            {
+                counts[i, j] = -1;
+            }
+        }
+
+        long count = Recursive(n, coins.Count - 1, coins, counts);
+
+        return count;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] firstLineOfInput = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = firstLineOfInput[0];
+        List<long> coins = Console.ReadLine().Split().Select(long.Parse).ToList();
+
+        Console.WriteLine(GetWays(n, coins));
+    }
+}
---
+++ Data-Structures-and-Algorithms/00.Demo/00.Demo.RodCutting/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    // Warm up for later https://www.hackerrank.com/challenges/equal/problem?isFullScreen=true
+    
+    public static long GetPrice(long n, long currentLength, long[] prices)
+    {
+        if(currentLength > n)
+        {
+            return 0;
+        }
+
+        if(n <= 0) 
+        {
+            return 0;
+        }
+
+        long currentPrice = prices[currentLength];
+
+        long remainingPrice = GetPrice(n - currentLength, currentLength, prices);
+        long otherPrice = GetPrice(n, currentLength + 1, prices);
+
+        return Math.Max(currentPrice + remainingPrice, otherPrice);
+    }
+    
+    public static void Main(string[] args)
+    {
+        long[] prices = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30, 31, 34, 44, 50, 60, 70, 78, 90, 102, 120, 135, 145, 175, 200, 250, 300 };
+
+        Console.WriteLine(prices.Length);
+
+        long n = long.Parse(Console.ReadLine());
+
+        Console.WriteLine(GetPrice(n, 1, prices));
+    }
+}
