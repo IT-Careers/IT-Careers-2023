9f6913513b0a35f6c895cf1af67e35625bd85b9d
DCay <atriumandvintrigol@gmail.com>
2022-11-19 11:46:06 +0200

    Added additional functions

--- Functional-Programming/HaskeLinq.hs
+++ Functional-Programming/HaskeLinq.hs
@@ -7,3 +7,33 @@ reverseList' list result n =
 
 reverseList list = reverseList' list [] (length list) 
 
+mapList mappingFunc list = 
+    if null list
+        then []
+    else [mappingFunc $ head list] ++ (mapList mappingFunc (tail list))
+
+filterList filterFunc list =
+    if null list
+        then []
+    else if not (filterFunc (head list))
+        then filterList filterFunc (tail list)
+    else [head list] ++ filterList filterFunc (tail list)
+
+enumerate start end =
+    if start > end
+        then []
+    else [start] ++ enumerate (start + 1) end
+
+get list index = 
+    if index == 0
+        then (head list)
+    else get (tail list) (index - 1)
+
+reduce reducerFunc accumulator list = 
+    if null list
+        then accumulator
+    else reduce reducerFunc (reducerFunc accumulator (head list)) (tail list)
+
+generate' n = [n] ++ generate' (n + 1) 
+
+generate = generate' 1
--- Functional-Programming/main.hs
+++ Functional-Programming/main.hs
@@ -2,9 +2,7 @@ import StringUtils;
 import HaskeLinq;
 import PredefinedFunctions;
 
-processCommand command = do
-    let params = splitStr command " "
-    print $ reverseList params
-
 main = do
-    readUntil "end" processCommand
+    print $ zipWith (\x y -> x + y) [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+    
